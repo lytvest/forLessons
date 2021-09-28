7cd6805997b6dc0dc8eb05cf1a7c4ef7a7ad82d0
Дмитрий Башкирцев <bdm1999@yandex.ru>
2021-09-28 20:18:05 +0500

    lesson5 2 checkpoint

--- src/lesson5/SimpleDotComTestDrive.java
+++ src/lesson5/SimpleDotComTestDrive.java
@@ -4,28 +4,38 @@ public class SimpleDotComTestDrive {
     public static void main(String[] args) {
         SimpleDotCom dot = new SimpleDotCom();
         int [] location = {2, 3, 4};
-
         dot.setLocationCells(location);
-        String userGuess = "1";
-        String result = dot.checkYourself(userGuess);
-        String testResult = "Неудача";
-        if (result.equals("Попал")){
-            testResult = "Пройден";
+        boolean isAlive = true;
+        int numOfGuesses = 0;
+        GameHelper gm = new GameHelper();
+        while (isAlive) {
+            numOfGuesses++;
+            String userGuess = gm.getUserInput("Введите число :");
+            String result = dot.checkYourself(userGuess);
+            System.out.println(result);
+            if(result.equals("Потопил")){
+                isAlive = false;
+            }
         }
-        System.out.println(testResult);
+        System.out.println("Вы справились за " + numOfGuesses + " попытки");
     }
 }
 
 class SimpleDotCom {
     int [] locations;
+    int numOfHints = 0;
     void setLocationCells(int [] locations){
         this.locations = locations;
     }
 
     String checkYourself(String guess){
         for(int i: locations){
-            if (i == Integer.parseInt(guess))
+            if (i == Integer.parseInt(guess)) {
+                numOfHints++;
+                if (numOfHints == locations.length)
+                    return "Потопил";
                 return "Попал";
+            }
         }
         return "Мимо";
     }
