3843138757059e947f883666ae4b0ef861901ebc
Дмитрий Башкирцев <bdm1999@yandex.ru>
2021-11-16 19:16:43 +0500

    lesson 11

---
+++ src/lesson11/StaticTests.java
@@ -0,0 +1,32 @@
+package lesson11;
+
+class StaticSuper {
+    static {
+        System.out.println("Родительский статический блок");
+    }
+
+    StaticSuper() {
+        System.out.println("Родительский конструктор");
+    }
+    final void method(){
+
+    }
+}
+
+public class StaticTests extends StaticSuper {
+
+    static int rand;
+    static {
+        rand = (int) (Math.random() * 6);
+        System.out.println("Статический блок " + rand);
+    }
+
+    StaticTests() {
+        System.out.println("Конструктор");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Внутри main");
+        StaticTests st = new StaticTests();
+    }
+}
